98277a15d6e566b32a0539331ce297dccfe457d1
Takuya Mukohira <takuya.mk96@gmail.com>
2015-09-03 18:06:10 +0900

    Add to_html

--- geppo.rb
+++ geppo.rb
@@ -4,6 +4,7 @@ require 'nokogiri'
 class Geppo
   def run
     entries = fetch_entries
+    puts to_html(entries)
   end
 
   private
@@ -23,6 +24,30 @@ class Geppo
       entries.push(entry)
     end
     return entries
+  end
+
+  def to_html(entries)
+    html = "<h3>タイムライン</h3>\n"
+
+    entries.reverse.each_with_index do |entry, i|
+      if i == 0 || entries[i - 1][:pub_date] != entry[:pub_date]
+        html += "<h4>#{entry[:pub_date].day.to_s}日（#{%w(日 月 火 水 木 金 土)[entry[:pub_date].wday]}）</h4>\n"
+        html += "<ul>\n"
+      end
+
+
+      html += "<li>"
+      html += "<a style=\"line-height: 1.5;\" href=\"#{entry[:link]}\">"
+      html += "#{entry[:title]}"
+      html += "</a>"
+      html += "</li>\n"
+
+      if entries[i + 1].nil? || entries[i + 1][:pub_date] != entry[:pub_date]
+        html += "</ul>\n"
+      end
+    end
+
+    return html
   end
 end
 
